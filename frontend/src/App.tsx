08afe5a8d675c361df030df0595c73f2631e5361
Muhammed Abdrahman <muhammed03amin@gmail.com>
2024-07-10 15:14:44 +0500

    feat: add default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Products from "./pages/Products";
 import CreateProduct from "./pages/CreateProduct";
 import EditProduct from "./pages/EditProduct";
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/create" element={<CreateProduct />} />
         <Route path="/products/edit/:id" element={<EditProduct />} />
